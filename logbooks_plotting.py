bbd8f0b32a43a8e792000182c9aa1dbda523f558
sofaw <soph.wood52@gmail.com>
2018-01-17 16:04:13 +0000

    Add plotting for box and whisker diagrams.

--- logbooks_plotting.py
+++ logbooks_plotting.py
@@ -14,7 +14,6 @@ def get_line(logbooks, colour, label, axis):
     return axis.plot(gen, fit_max, colour, label=label)
 
 
-
 def plot_max_from_logbooks(logbooks_path_list, colours_list, label_list):
     fig, ax1 = plt.subplots()
 
@@ -32,6 +31,45 @@ def plot_max_from_logbooks(logbooks_path_list, colours_list, label_list):
 
     plt.show()
 
+def column(matrix, i):
+    return [row[i] for row in matrix]  #https://stackoverflow.com/questions/903853/how-do-you-extract-a-column-from-a
+    # -multi-dimensional-array
+
+
+def gen_boxplot(logbooks, colour, plot):
+    numRuns = len(logbooks)
+    numGens = len(logbooks[0])
+
+    # Get max at each generation for each run
+    fit_max_by_run = [0] * numRuns
+    for i in range(len(logbooks)):
+        fit_max_by_run[i] = logbooks[i].select("max")
+
+    fit_max_by_gen = [0] * numGens
+    for i in range(numGens):
+        fit_max_by_gen[i] = column(fit_max_by_run, i)
+
+    boxplots = plot.boxplot(fit_max_by_gen, patch_artist=True)
+
+    for patch in boxplots['boxes']:
+        patch.set_facecolor(colour)
+
+
+
+def plot_box_and_whisker_from_logbooks(logbooks_path_list, colours_list):
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel("Generation")
+    ax1.set_ylabel("Fitness")
+
+    for i in range(len(logbooks_path_list)):
+        logbooks = pickle.load(open(logbooks_path_list[i], "rb"))
+        gen_boxplot(logbooks, colours_list[i], plt)
+
+    plt.show()
+
 
 # Testing
 #plot_max_from_logbooks(["dumps/logbook.p", "dumps/adf_logbook.p"], ["y", "b"], ["Without ADF", "ADF"])
+plot_box_and_whisker_from_logbooks(["dumps/logbook.p", "dumps/adf_logbook.p"], ["y", "b"])
+#plot_box_and_whisker_from_logbooks(["dumps/logbook.p"], ["y"])
